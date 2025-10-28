aaaaff1ac3aa35a6c309c72b98ec6effe12a0cae
unknown <tamrynripepi00@gmail.com>
2025-10-28 14:52:10 +0200

    Initial commit

---
+++ basics.js
@@ -0,0 +1,10 @@
+const firstName = "Tamryn";
+const lastName = "Ripepi";
+let age = 25;
+
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(age);
+
+age = 26;
+console.log(`I will be ${age} next year`);
